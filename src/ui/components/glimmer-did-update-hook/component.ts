9edddc98f151ae820f06213695870b53a05a3a7b
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-04-11 12:58:25 -0400

    didUpdate called before DOM rerendered

--- src/ui/components/glimmer-did-update-hook/component.ts
+++ src/ui/components/glimmer-did-update-hook/component.ts
@@ -1,5 +1,28 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class GlimmerDidUpdateHook extends Component {
+  updated = false;
+
+  @tracked
+  foo: boolean = false;
+
+  didInsertElement() {
+    setTimeout(() => {
+      this.foo = true;
+    }, 2000);
+  }
+
+
+  didUpdate() {
+    if (!this.updated) {  // to compensate for infinite rerender bug
+      this.updated = true;
+
+      console.log('this.foo is ', this.foo);
+      let fooElement = document.getElementById("foo");
+      if (!fooElement) {
+        console.log("Can't find foo");
+      }
+    }
+  }
 
 }
